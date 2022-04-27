0e9079e8ac3d70f00695f42b5ed685edd4fb5b54
Boris Kaus <kaus@uni-mainz.de>
2022-04-27 23:21:51 +0200

    compute average T for tracers with T>700

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,4 +1,5 @@
 
+ using Statistics: mean
 
 """
     Process_ZirconAges(dirname; ZirconData=ZirconAgeData()) 
@@ -25,9 +26,21 @@ function Process_ZirconAges(dirname; ZirconData=ZirconAgeData())
     filename = dirname*"/Tracers_SimParams.jld2"
     Tracers, Tav_magma_Time, Time_vec = JLD2.load(filename,"Tracers","Tav_magma_Time","Time_vec")
 
+    # Compute average T in magma (that is all tracers with T>700)
+    time_years, Ttpaths_mat = compute_zircons_convert_vecs2mat(Tracers.time_vec, Tracers.T_vec)
+    Ttpaths_mat[Ttpaths_mat.<700] .= 0;
+    T_average_magma_time = zero(time_years)
+    for i = 1:length(time_years)
+        T_vec = Ttpaths_mat[i,:];
+        T_average_magma_time[i] = mean(T_vec[T_vec .> 10])
+    end
+
     # Compute zircon ages
     time_Ma, PDF_zircons, time_Ma_average, PDF_zircon_average, time_years, prob, ages_eruptible, number_zircons, T_av_time, T_sd_time = 
         compute_zircon_age_PDF(Tracers.time_vec*1e6, Tracers.T_vec; ZirconData = ZirconData, bandwidth=1e5, n_analyses=300);
+
+
+
 
     # compute useful vectors (for plotting): 
     Age_Ma = time_years[end:-1:1]/1e6;   # age of the rocks (opposite of model time)
@@ -38,7 +51,7 @@ function Process_ZirconAges(dirname; ZirconData=ZirconAgeData())
     filename_save = dirname*"/ZirconAges.jld2"
     jldsave(filename_save; time_Ma, PDF_zircons, time_Ma_average, PDF_zircon_average, 
                             time_years, prob, ages_eruptible, number_zircons, T_av_time, T_sd_time, Tav_magma_Time, 
-                            Age_Ma, cum_PDF, norm_PDF, Time_vec)
+                            Age_Ma, cum_PDF, norm_PDF, Time_vec, T_average_magma_time)
     println("Save processed zircon age data to file: $filename_save")
 
     return nothing
