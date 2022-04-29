b03cf38c17abc20eb8867ae3cc629b55fbe0897a
Boris Kaus <kaus@uni-mainz.de>
2022-04-29 16:10:11 +0200

    update test

--- test/test_Grid.jl
+++ test/test_Grid.jl
@@ -1,7 +1,7 @@
 using Test
 using MagmaThermoKinematics
-
 environment!(:cpu, Float64, 2) 
+using MagmaThermoKinematics.Diffusion2D
 
 @testset "Grid" begin
 
@@ -13,13 +13,13 @@ Grid = CreateGrid(size=(10,20),x=(0.,10), z=(2.,10))
 @test Grid.Δ[1] ≈ 1.1111111111111112
 @test Grid.Δ[2] ≈ 0.42105263157894735
 
-X,Z = GridArray(Grid.coord1D[1], Grid.coord1D[2])
+
+X = @zeros(Grid.N...)
+Z = @zeros(Grid.N...)
+@parallel (1:Grid.N[1], 1:Grid.N[2]) GridArray!(X,Z,Grid.coord1D[1], Grid.coord1D[2])
+
 @test sum(X) ≈ 1000
 @test minimum(Z) ==2.0
-
-X .= 0.0;
-GridArray!(X,Z,Grid.coord1D[1], Grid.coord1D[2])
-@test sum(X[:,1]) == 50.0
 
 
 end
